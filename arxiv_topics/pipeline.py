737bee19bb83c5bdf0fb3f9a77c18d2aa9171a01
Martin Sander <156700437+smartIU@users.noreply.github.com>
2024-06-03 13:05:13 +0200

    Update pipeline.py

--- arxiv_topics/pipeline.py
+++ arxiv_topics/pipeline.py
@@ -77,7 +77,7 @@ class Pipeline:
          
          ## LLM
          
-         #chained "Main" representation model plus aspect needs a fix in bertopic module - see https://github.com/MaartenGr/BERTopic/pull/2021
+         #chained "Main" representation model plus aspect is not supported
          #self.representation_model = [KeyBERTInspired(top_n_words=bert_params.top_n_words), MaximalMarginalRelevance(diversity=bert_params.keyword_diversity)]
          self.representation_model = MaximalMarginalRelevance(diversity=bert_params.keyword_diversity)                     
 
