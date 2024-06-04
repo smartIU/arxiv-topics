225df5d90ac6538a327901ea9aac580625592361
Martin Sander <156700437+smartIU@users.noreply.github.com>
2024-06-04 19:10:18 +0200

    Removed unnecessary imports

--- Step05_Regenerate_Labels.py
+++ Step05_Regenerate_Labels.py
@@ -1,45 +1,35 @@
-import pandas as pd
-import numpy as np
-import gc
-
 from arxiv_topics.config import Config
 from arxiv_topics.db import DB
 from arxiv_topics.pipeline import Pipeline
 
 
 if __name__ == "__main__":
-    """ use LLM model to regenerate labels
-        only affects topics without a label, so for unappealing results first manually set the llm_label in the sqlite database to an empty string
-    """
+    """ creates evaluations for a specific model, if not activated in Step04_Train_Models """
+
+
+    #TODO: Make arg
+    model = 'arxiv_0'
+    
+    create_heatmap = True
+    create_barchart = True
+    create_hierarchy = True
+
 
     config = Config()
 
-    db = DB(config.database)
+    db = DB(config.database)    
+
+    if create_hierarchy:
+        print(f'collecting papers')    
+        abstracts = db.get_papers_by_model(model)['abstract']
+    else:
+        abstracts = None
 
 
-    # Get topics    
-    models = db.get_topics_without_label()
-        
-    for model in models:
+    print('loading model')    
+    topic_model = Pipeline.load_model(model)
 
-        model_name = model[0]
-
-        print(f'Loading model {model_name}')
-        topic_model = Pipeline.load_model(model_name)
-
-
-        print('Generating labels')
-        abstracts = model[1]['abstract']
-        topic_ids = model[1]['topic_id']
-
-        ids, labels = Pipeline.regenerate_labels(topic_model, abstracts, topic_ids
-                                               , config.llm_prompt, config.llm_diversity, config.llm_stopwords)
-
-        print('Updating database')
-        db.import_topic_labels(model_name, ids, labels)
-
-        # free memory
-        del topic_model
-        gc.collect()
+    # Evaluation
+    Pipeline.evaluate_model(topic_model, model, abstracts, create_heatmap, create_barchart, create_hierarchy)  
 
     print('Done.')
